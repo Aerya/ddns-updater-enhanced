15c0080121371168ed7eccd86953b25756d3e5cc
Aerya <blog@upandclear.org>
2026-07-17 22:52:31 +0200

    fix: retirer le middleware RealIP vulnérable au spoofing

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -49,7 +49,6 @@ func newHandler(ctx context.Context, rootURL string,
 
 	router := chi.NewRouter()
 
-	router.Use(middleware.RealIP)
 	router.Use(middleware.Logger)
 	rootURL = strings.TrimSuffix(rootURL, "/")
 
